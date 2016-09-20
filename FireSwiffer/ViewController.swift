05c1fef83d8a7d66a0f63ffda56cfdc2d0691398
Ovi Bortas <ovib313@gmail.com>
2016-09-19 21:10:36 -0400

    initial commit
    add project

---
+++ FireSwiffer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FireSwiffer
+//
+//  Created by Ovidiu Bortas on 9/19/16.
+//  Copyright © 2016 Ovidiu Bortas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
